c76bc645c02529e6afb5fc9ba74c4ba1d6254b33
Nashbi Grand-Jean <nhgrandjean1@gmail.com>
2022-08-15 18:43:55 -0400

    updates

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
+// Including packages needed to produce this work
 const fs = require('fs');
 const path = require('path');
 const inquirer = require ('inquirer');
 
 
+
+const questions = [
+
+{
+    type: "input",
+    message: "What is the team manager's name?",
+    name: "title",
+}, 
+{
+    type: "input",
+    message: "What is your employee ID?",
+    name: "title",
+}, 
+{
+    type: "input",
+    message: "What is your email address?",
+    name: "title",
+}, 
+{
+    type: "input",
+    message: "What is your office number?",
+    name: "title",
+}, 
+
+];
---
+++ tests/employee.test.js
@@ -0,0 +1,30 @@
+
+describe("employee", () => {
+        
+    it("Should include an employee id", () => {
+
+    const id = 1 
+
+    expect(employee.id).toEqual(id)
+
+})});
+
+describe("employee", () => {
+        
+    it("Should include an employee email", () => {
+
+    const email = 1 
+
+    expect(employee.id).toEqual(id)
+
+})});
+
+describe("employee", () => {
+        
+    it("Should include an employee id", () => {
+
+    const id = 1 
+
+    expect(employee.id).toEqual(id)
+
+})});
